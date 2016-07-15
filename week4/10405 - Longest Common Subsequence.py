1c7c2402cd3b018f17d0c7501c294958534fac76
mschonfinkel <marcos.schonfinkel@gmail.com>
2016-07-14 23:20:05 -0300

    Minor changes in LCS

--- week4/10405 - Longest Common Subsequence.py
+++ week4/10405 - Longest Common Subsequence.py
@@ -1,19 +1,20 @@
 def lcs(s1, s2):
 	# Longest Common Subsequence
 	lengths = dict()
+	get = lengths.get
 	for i, c1 in enumerate(s1):
 		for j, c2 in enumerate(s2):
 			if c1 == c2:
-				lengths[i+1, j+1] = lengths.get((i,j), 0) + 1
+				lengths[i+1, j+1] = get((i,j), 0) + 1
 			else:
-				lengths[i+1, j+1] = max(lengths.get((i+1,j), 0), lengths.get((i,j+1), 0))
-	
+				lengths[i+1, j+1] = max(get((i+1,j), 0), get((i,j+1), 0))
+
 	result = []
 	x, y = len(s1), len(s2)
 	while x != 0 and y != 0:
-		if lengths.get((x,y), 0) == lengths.get((x-1,y), 0):
+		if get((x,y), 0) == get((x-1,y), 0):
 			x -= 1
-		elif lengths[x, y] == lengths.get((x,y-1), 0):
+		elif lengths[x, y] == get((x,y-1), 0):
 			y -= 1
 		else:
 			result = [s1[x-1]] + result
